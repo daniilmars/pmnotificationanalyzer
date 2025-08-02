c6a87172a786b701323ec7cfcfe226ca43a75739
daniilmars <daniilmarszallek@yahoo.de>
2025-08-02 12:23:50 +0200

    Change

--- pm-analyzer-fiori/webapp/Component.js
+++ pm-analyzer-fiori/webapp/Component.js
@@ -20,15 +20,20 @@ function (UIComponent, Device, JSONModel, MessageBox, ResourceModel) {
             // Set the device model
             this.setModel(new JSONModel(Device), "device");
 
-            // --- START: NEW DATA MODEL INITIALIZATION ---
+            // --- START: CORRECTED DATA MODEL INITIALIZATION ---
             // Explicitly create and set the main data model
             var oModel = new JSONModel();
             this.setModel(oModel); // Set as the default model
 
+            // Wait for the data to be loaded before initializing the router
+            oModel.dataLoaded().then(() => {
+                this.getRouter().initialize();
+            });
+
             // Load the mock data into the model
             var sMockDataPath = sap.ui.require.toUrl("com/sap/pm/pmanalyzerfiori/mock_data_en.json");
             oModel.loadData(sMockDataPath);
-            // --- END: NEW DATA MODEL INITIALIZATION ---
+            // --- END: CORRECTED DATA MODEL INITIALIZATION ---
 
 
             // Check for stored language and set it
@@ -45,14 +50,11 @@ function (UIComponent, Device, JSONModel, MessageBox, ResourceModel) {
 
             // Initialize the i18n model
             const i18nModel = new ResourceModel({
-                bundleName: "com.sap.pm.pmanalyzerfiori.i18n.i18n",
+                bundleName: "com.sap/pm/pmanalyzerfiori.i18n.i18n",
                 supportedLocales: ["en", "de"],
                 fallbackLocale: "en"
             });
             this.setModel(i18nModel, "i18n");
-
-            // Initialize the router
-            this.getRouter().initialize();
         }
     });
 });
